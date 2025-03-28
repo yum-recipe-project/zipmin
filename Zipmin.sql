a6416750ec1991eaa74a8453d1f462a028561934
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2025-03-29 00:31:39 +0900

    docs: 데이터베이스 파일 수정

--- Zipmin.sql
+++ Zipmin.sql
@@ -10,8 +10,8 @@ drop table chomp_megazine;
 drop sequence seq_chomp_megazine_id;
 drop table chomp_vote_record;
 drop sequence seq_chomp_vote_record_id;
-drop table chomp_vote_option;
-drop sequence seq_chomp_vote_option_id;
+drop table chomp_vote_choice;
+drop sequence seq_chomp_vote_choice_id;
 drop table chomp_vote;
 drop sequence seq_chomp_vote_id;
 drop table chomp;
@@ -28,6 +28,15 @@ drop table comments;
 drop table seq_comments_id;
 drop table guide;
 drop sequence seq_guide_id;
+
+drop table recipe_step;
+drop sequence seq_recipe_step_id;
+drop table recipe_ingredient;
+drop table sequence seq_recipe_ingredient_id;
+drop table recipe_category;
+drop sequence seq_recipe_category_id;
+drop table recipe;
+drop sequence seq_recipe_id;
 
 drop table user_account;
 drop sequence seq_user_account_id;
@@ -83,22 +92,109 @@ commit;
 
 
 
--- RECIPE 테이블
 
+
+
+-- RECIPE 테이블
+-- drop table recipe;
+-- drop sequence seq_recipe_id;
+create table recipe (
+    id number primary key,
+    image_url varchar2(50) not null,
+    title varchar2(50) not null,
+    introduce varchar2(300) not null,
+    cooklevel varchar2(15) not null,
+    cooktime varchar2(15) not null,
+    spicy varchar2(15) not null,
+    portion varchar2(15) not null,
+    tip varchar2(300),
+    youtube_url varchar2(100),
+    user_id varchar2(15) not null
+);
+alter table recipe
+    add constraint const_recipe_users foreign key(user_id)
+    references users(id) on delete cascade;
+create sequence seq_recipe_id
+    increment by 1
+    start with 1
+    minvalue 1
+    nomaxvalue
+    nocycle
+    nocache;
+commit;
 
 
 
 -- RECIPE_CATEGORY 테이블
-
+-- drop table recipe_category;
+-- drop sequence seq_recipe_category_id;
+create table recipe_category (
+    id number primary key,
+    type varchar2(15) not null,
+    tag varchar2(15) not null,
+    recipe_id number not null
+);
+alter table recipe_category
+    add constraint const_recipe_category_recipe foreign key(recipe_id)
+    references recipe(id) on delete cascade;
+create sequence seq_recipe_category_id
+    increment by 1
+    start with 1
+    minvalue 1
+    nomaxvalue
+    nocycle
+    nocache;
+commit;
 
 
 
 -- RECIPE_INGREDIENT 테이블
-
+-- drop table recipe_ingredient;
+-- drop table sequence seq_recipe_ingredient_id;
+create table recipe_ingredient (
+    id number primary key,
+    name varchar2(50) not null,
+    amount number not null,
+    unit varchar2(30) not null,
+    note varchar2(300),
+    recipe_id number not null
+);
+alter table recipe_ingredient
+    add constraint const_recipe_ingredient_reicpe foreign key(recipe_id)
+    references recipe(id) on delete cascade;
+create sequence seq_recipe_ingredient_id
+    increment by 1
+    start with 1
+    minvalue 1
+    nomaxvalue
+    nocycle
+    nocache;
+commit;
 
 
 
 -- RECIPE_STEP 테이블
+-- drop table recipe_step;
+-- drop sequence seq_recipe_step_id;
+create table recipe_step (
+    id number primary key,
+    image_url varchar2(100) not null,
+    content varchar2(500) not null,
+    recipe_id number
+);
+alter table recipe_step
+    add constraint const_recipe_step_recipe foreign key(recipe_id)
+    references recipe(id) on delete cascade;
+create sequence seq_recipe_step_id
+    increment by 1
+    start with 1
+    minvalue 1
+    nomaxvalue
+    nocycle
+    nocache;
+commit;
+
+
 
 
 
@@ -216,7 +312,7 @@ create table report (
 );
 alter table report
     add constraint const_report_users foreign key(user_id)
-    references users(user_id) on delete set null;
+    references users(id) on delete set null;
 create sequence seq_report_id
     increment by 1
     start with 1
@@ -247,8 +343,18 @@ commit;
 
 
 -- FUND 테이블
+/**
+create table fund (
+    id number primary key,
+    funder_id varchar2(15),
+    fundee_id varchar2(15),
+    recipe_id number not null,
+    point number not null,
+    funddate date default sysdate not null,
+    status number default 0 not null
+);
 
-
+*/
 
 
 
@@ -294,18 +400,18 @@ commit;
 
 
 
--- CHOMP_VOTE_OPTION 테이블
--- drop table chomp_vote_option;
--- drop sequence seq_chomp_vote_option_id;
-create table chomp_vote_option (
+-- CHOMP_VOTE_CHOICE 테이블
+-- drop table chomp_vote_choice;
+-- drop sequence seq_chomp_vote_choice_id;
+create table chomp_vote_choice (
     id number primary key,
     vote_id number not null,
-    option varchar2(100) not null
+    choice varchar2(100) not null
 );
-alter table chomp_vote_option
-    add constraint const_chomp_vote_option_chomp_vote foreign key(vote_id)
+alter table chomp_vote_choice
+    add constraint const_chomp_vote_choice_chomp_vote foreign key(vote_id)
     references chomp_vote(id) on delete cascade;
-create sequence seq_chomp_vote_option_id
+create sequence seq_chomp_vote_choice_id
     increment by 1
     start with 1
     minvalue 1
@@ -323,7 +429,7 @@ create table chomp_vote_record (
     id number primary key,
     vote_id number not null,
     user_id varchar2(15),
-    option_id number not null
+    choice_id number not null
 );
 alter table chomp_vote_record
     add constraint const_chomp_vote_record_chomp_vote foreign key(vote_id)
@@ -332,8 +438,8 @@ alter table chomp_vote_record
     add constraint cosnt_chomp_vote_record_users foreign key(user_id)
     references users(id) on delete set null;
 alter table chomp_vote_record
-    add constraint const_chomp_vote_record_chomp_vote_option foreign key(option_id)
-    references chomp_vote_option(id) on delete cascade;
+    add constraint const_chomp_vote_record_chomp_vote_choice foreign key(choice_id)
+    references chomp_vote_choice(id) on delete cascade;
 create sequence seq_chomp_vote_record_id
     increment by 1
     start with 1

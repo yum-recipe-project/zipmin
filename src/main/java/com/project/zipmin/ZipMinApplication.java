30e830513d0a86ad1adde2413e65bba1e7243955
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2025-04-26 22:50:34 +0900

    chore: Ignore application.properties

--- src/main/java/com/project/zipmin/ZipMinApplication.java
+++ src/main/java/com/project/zipmin/ZipMinApplication.java
@@ -1,16 +1,16 @@
 package com.project.zipmin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 
 @SpringBootApplication
-
+@MapperScan(basePackages = "com.project.zipmin.mapper")
 public class ZipMinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZipMinApplication.class, args);
-		
 		// String password = PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("1234");
 	}
 

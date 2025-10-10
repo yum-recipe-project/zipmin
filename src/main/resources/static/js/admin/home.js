3a25fb4c3bb70f05482c4b70f7fa9c0b430736b3
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2025-10-11 00:40:52 +0900

    fix: 관리자 페이지 진입 접근 권한 수정

---
+++ src/main/resources/static/js/admin/home.js
@@ -0,0 +1,13 @@
+/**
+ * 접근 권한을 설정하는 함수
+ */
+document.addEventListener('DOMContentLoaded', async function() {
+
+	try {
+		await instance.get('/dummy');
+	}
+	catch (error) {
+		redirectToLogin('/');
+	}
+	
+});

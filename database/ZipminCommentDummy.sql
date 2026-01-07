1396d048fafde9c69dbd396f50caa71c99d21819
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2026-01-08 01:18:38 +0900

    docs: 키친가이드 댓글 더미데이터 작성

---
+++ database/ZipminCommentDummy.sql
@@ -0,0 +1,77 @@
+-- ============
+-- 키친가이드 댓글
+-- ============
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '식초물에 담구는건 몰랐어요 감사합니다', 'guide', 1, 4);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, null, sysdate, '닭고기를 꼭 씻어 사용하신다면 볼에 물을 담고 살살 문질러 씻고 끓는 물에 1분이상 끓여 세균을 제거한 후 씻어 사용하시는 방법도 있어요^^', 'guide', 2, 4);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '까나리의 재발견', 'guide', 5, 5);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '그럼 모든 요리에 소금 대신 액젓을 사용하면 건강에 더 좋은건가요?', 'guide', 5, 14);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '멸치액젓만 썼는데 까나리액젓 쓰임새 너무 좋네요', 'guide', 5, 4);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '염장 다시마 손질법도 알려주실 수 있나요?', 'guide', 7, 4);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '오렌지 의외로 금방 물러서 안타까웠는데 오래 먹을 수 있을 것 같아요', 'guide', 8, 7);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '쉽네요!', 'guide', 8, 6);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '오 밀가루가 세척 비법이었네요!', 'guide', 9, 4);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '좋은 방법이네요 근데 기간이 지나면 음식이 상하나요?', 'guide', 10, 7);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '당근 생으로 냉동 2개월 맞죵?', 'guide', 10, 13);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '랩은 알았는데 종이타올도 이용하는건 처음알았어요', 'guide', 10, 4);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '한치 좋아하는데 감사합니다!', 'guide', 11, 9);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '밀가루로 씻어도 되쥬?', 'guide', 11, 6);
+
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '항상 딱딱한 거 자르느라 힘들었는데 꿀팁이네요.', 'guide', 13, 1);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '전자렌지 없을 때 손질법도 알려주시면 유용할 것 같아요!', 'guide', 13, 5);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '맛있겠네요', 'guide', 13, 6);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '잘라서 보관하면 해동 후 따로 손질할 필요도 없겠네요', 'guide', 12, 4);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '호일을 씌운다는 생각을 한번도 해보지도 다른 곳에서 본 적도 없었는데 도움이 많이 되었네요... 감사합니다', 'guide', 14, 6);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '꼬막까기 진짜 짱', 'guide', 14, 4);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '감사합니다 쉽고 재밌어요', 'guide', 14, 5);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '너무 필요했던 간편 양념!', 'guide', 15, 5);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '곤드레밥해서 간장 양념에 맛있게 비벼 먹었어요~', 'guide', 15, 8);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '만능양념 잘 사용하고 있습니다!', 'guide', 15, 4);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '고추장 양념에는 참기름이 안들어가네요 이유가 있나요? 요알못이라 참기름을 넣는지 안넣는지가 너무 헷갈려요', 'guide', 15, 9);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '감사합니다 요긴하게 잘 사용하고 있어요', 'guide', 15, 6);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '소금 조절을 못해서 좀 짰지만 털어서 맛있게 먹었어요~', 'guide', 17, 6);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '들기름이 없어서 참기름으로 했어요', 'guide', 17, 7);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '김밥김으로 해도 되나요?', 'guide', 17, 4);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '밀가루 없어서 냉동실에 있는 튀김가루로 씻었어요~ 덕분에 낙지 손질 잘했습니다', 'guide', 18, 11);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '소금도 넣으면 이물질이 확실히 제거 되겠지만 짜지지 않은지...', 'guide', 18, 9);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '너무 큰 낙지를 사서 무서웠지만 깨끗하게 손질 되었어요 감사해요', 'guide', 18, 8);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '밀가루 대신 밀가루 비슷한 가루로 벅벅 씻었어요 낙지 살짝 데쳐먹으니 아주 부드러웠어요', 'guide', 18, 4);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '처음으로 한 연포탕!', 'guide', 18, 14);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '쭈꾸미 손질도 이렇게 하면 되나요?', 'guide', 18, 5);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '실수로 전자렌지 3분 30초 정도 돌렸는데 다 탔어요', 'guide', 19, 7);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '특유의 냄새 없이 정말 맛있어요 감사합니다', 'guide', 19, 4);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '전자렌지 전력에 따라 다르게 돌려야될 것 같아요 1000W 이상은 30초 정도면 적당할 듯 하네요', 'guide', 19, 5);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '오 칼집 전혀 생각 못했어요 감사합니다', 'guide', 21, 10);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '감자 자체가 맛있어서 그런지 너무 맛있어요', 'guide', 21, 5);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '간단하게 먹을거 없나 해서 먹었는데 너무 맛있어요', 'guide', 21, 13);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '감자를 칼집내니 껍질까기 쉬워서 너무 좋아요', 'guide', 21, 4);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '칼집내면 굳이 힘들게 안깍아도 되는군요', 'guide', 21, 7);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '감사합니다~', 'guide', 22, 5);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '시골에서 상추를 엄청 많이 가져왔는데 이렇게 보관해봐야겠어요', 'guide', 23, 4);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '노하우라고 하기엔 특별한 방법은 아니네요 이미 다 그렇게 하고 있는데...', 'guide', 23, 10);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '이렇게 하면 오래가나요', 'guide', 23, 9);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '그냥 물기를 탈수기에 완전히 제거하고 비닐봉지에 넣어 야채칸에 보관해도 충분합니다!', 'guide', 23, 4);
+
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '요리할 때마다 하나씩 꺼내쓰기 좋아요', 'guide', 24, 4);
+INSERT INTO comments VALUES (seq_comments_id.NEXTVAL, NULL, SYSDATE, '감사합니다~ 잘 봤어요', 'guide', 24, 6);
+
+
+
+
+
+COMMIT;
